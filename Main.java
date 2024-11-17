49b7c528fce20d56e4030e0621e1a22b599d63d1
Anurag Gupta <anuraggangil@gmail.com>
2024-11-17 13:43:20 +0530

    add student class

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ public static void main(String[] args) {
     System.out.println("This is java program");
     System.out.println("Today date is : "+new Date());
     System.out.println("Add one more file");
+    Student appu= new Student();
+    appu.sayHello();
 }
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating Student object");
+    }
+    public void sayHello(){
+        System.out.println("Hello coder");
+    }
+}
